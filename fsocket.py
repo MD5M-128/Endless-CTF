dd48daed9f77bdd16f051787143bbc121fdd51d2
Thegranite32 <31848971+Thegranite32@users.noreply.github.com>
2020-02-01 15:25:38 +0800

    Connected the fake socket to the virtual DNS

--- fsocket.py
+++ fsocket.py
@@ -12,34 +12,75 @@ def _exit_handler():
 
 atexit.register(_exit_handler)
 
+def ipv4_to_num(ip):
+    s = 0
+    p = 3
+    if len(splt := ip.split(".")) == 4:
+        for i in splt:
+            if 0 <= (n := int(i)) <= 255:
+                s += n * 256 ** p
+                p -= 1
+            else:
+                raise InvalidIPError("Each part of an IPv4 address must be between 0 and 255.")
+    else:
+        raise InvalidIPError("An IPv4 address may only have 4 parts")
+    return s
+def num_to_ipv4(num):
+    parts = []
+    for i in range(3, -1, -1):
+        parts.append(str(num >> (i * 8) & 255))
+    return ".".join(parts)
+def valid_ip_ipv4(ip):
+    try:
+        ipv4_to_num(ip)
+        return True
+    except InvalidIPError:
+        return False
+def valid_ip_num(num):
+    try:
+        ipv4_to_num(num_to_ipv4(num))
+        return True
+    except InvalidIPError:
+        return False
+
+class InvalidIPError(Exception):
+    pass
+
 class socket:
     def __init__(self, ws_addr=("localhost", 8765)):
         self._kill = False
-        self.ip = "127.0.1.1"
+        self.ip = ipv4_to_num("127.0.1.1")
         self.port = 1
         self._to_send = []
+        self._to_recv = []
+        self._bound_to = ("0.0.0.0", -1)
         SOCKETS.add(self)
 
         def on_message(ws, message):
-            print(message)
+            try:
+                msg = json.loads(message)
+                if (msg["ip"] == self._bound_to[0] or self._bound_to[0] == "0.0.0.0") and (msg["port"] == self._bound_to[1] or self._bound_to[1] == -1):
+                    if valid_ip_num(msg["ip"]):
+                        self._to_recv.append()
+                    else:
+                        print("Bound IP invalid!")
+            except json.decoder.JSONDecodeError:
+                print("Invalid JSON data sent!")
 
         def on_error(ws, error):
-            print(error)
+            raise error
 
         def on_close(ws):
-            print("### closed ###")
+            self._kill = True
+            del self
 
         def on_open(ws):
-            def run(*args):
-                while True:
-                    if self._kill: break
-                    if len(self._to_send) > 0:
-                        print(self._to_send[0])
-                        ws.send(json.dumps(self._to_send[0]))
-                        self._to_send.pop(0)
-                ws.close()
-                print("thread terminating...")
-            run()
+            while True:
+                if self._kill: break
+                if len(self._to_send) > 0:
+                    ws.send(json.dumps(self._to_send[0]))
+                    self._to_send.pop(0)
+            ws.close()
             
 
         #websocket.enableTrace(True)
@@ -60,8 +101,26 @@ class socket:
             "type": "packet",
             "fromIp": self.ip,
             "fromPort": self.port,
-            "ip": addr[0],
+            "ip": ipv4_to_num(addr[0]),
             "port": addr[1],
             "content": message
+        }
+        self._to_send.append(msg)
+    
+    def close(self):
+        self._kill = True
+
+    def dns_login(self, name, password):
+        msg = {
+            "type": "dns_login",
+            "name": name,
+            "password": password
+        }
+        self._to_send.append(msg)
+    def dns_logout(self, name, password):
+        msg = {
+            "type": "dns_logout",
+            "name": name,
+            "password": password
         }
         self._to_send.append(msg)
