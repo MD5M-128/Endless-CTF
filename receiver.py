8e00d221c422a15fec2daa812016343adf7eed4b
Thegranite32 <31848971+Thegranite32@users.noreply.github.com>
2020-02-02 20:19:07 +0800

    Wrote part of receiver program (CURRENTLY BROKEN; NEEDS LOOKING INTO)

---
+++ receiver.py
@@ -0,0 +1,24 @@
+import fake_socket as socket
+from subprocess import call
+from sys import argv
+
+password = argv[1]
+domain = "watkins.com"
+serve = "receiver"
+
+s = socket.socket()
+
+if s.dns_lookup(serve + "." + domain)["type"] == "dns_notfound":
+    print("Logging in to DNS...")
+    response = s.dns_login(serve, domain, password)
+    if response["type"] != "dns_login":
+        print("DNS Login error")
+        # TODO: Find out why, then try again
+        exit(0)
+
+while True:
+    data = s.recv()
+    if data: print(data)
+
+# And it's as easy as that.
+# No. It really isn't
