edf1ae82c80f620a6e51dbbeb956c47eda3f46b6
Thegranite32 <31848971+Thegranite32@users.noreply.github.com>
2020-02-02 20:18:42 +0800

    Improved example program

--- program.py
+++ program.py
@@ -3,23 +3,29 @@ from sys import argv
 
 password = argv[1]
 payload = argv[2]
-domain = "waktins.com"
+domain = "watkins.com"
 serve = "sender"
 
 s = socket.socket()
 
-response = s.dns_login(serve, domain, password)
-if response["type"] != "dns_login":
-    # TODO: Find out why, then try again
-    exit(0)
+if s.dns_lookup(serve + "." + domain)["type"] == "dns_notfound":
+    print("Logging in to DNS...")
+    response = s.dns_login(serve, domain, password)
+    if response["type"] != "dns_login":
+        print("DNS Login error")
+        # TODO: Find out why, then try again
+        exit(0)
 
+print("Looking up receiver server...")
 endpoint = s.dns_lookup("receiver.watkins.com")
-if response["type"] != "dns_result":
+if endpoint["type"] != "dns_result":
+    print("DNS Lookup error.")
     # TODO: Find out why, then try again
     exit(0)
 
-ip = endpoint["ip"] # SERIOUS: Make fake_socket return string IP addresses in all cases
-s.sendto(payload, ip)
+ip = endpoint["ip"]
+print("Sending...")
+s.sendto(payload, (ip, 65535))
 
 # And it's as easy as that.
 # No. It really isn't
