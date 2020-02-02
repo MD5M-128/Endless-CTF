a20dd204a712b4dbbd41c2f7012c9b6649ef90b8
Thegranite32 <31848971+Thegranite32@users.noreply.github.com>
2020-02-02 18:33:49 +0800

    Created the random number generating and recording function

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from Crypto.Random import random
 from flask import *
+import bcrypt
+import json
 
 app = Flask(__name__)
 
+with open("dnsrecords.json", "r") as f: dns_cred = json.loads(f.read())
+
+flags = {}
 
 @app.route("/")
 def home():
@@ -14,5 +20,23 @@ def network():
 @app.route("/dns/")
 def dns():
     return render_template("dns.html")
+
+@app.route("/api/loghashrequest", methods=["POST"])
+def api_loghashrequest():
+    name = request.form["name"]
+    password = request.form["password"]
+    if name in dns_cred:
+        if bcrypt.checkpw(password.encode("UTF8"), dns_cred[name]["password"].encode("UTF8")):
+            num = random.randint(0, 2**64 - 1)
+            response = {
+                "type": "gen_success",
+                "rand": num
+            }
+            flags[num] = name
+            return jsonify(response)
+        else:
+            return jsonify({"type": "gen_badpassword"})
+    else:
+        return jsonify({"type": "gen_dnsnotregistered"})
 
 app.run("0.0.0.0", 8080)
