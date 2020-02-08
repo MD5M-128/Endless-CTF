ba20292c81f9f5680b2e0a1395558a1042d51fc2
Thegranite32 <31848971+Thegranite32@users.noreply.github.com>
2020-02-08 18:36:37 +0800

    Added hash checker

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 from Crypto.Random import random
+from hashlib import sha512
 from flask import *
+import threading
 import bcrypt
 import json
+import time
 
 app = Flask(__name__)
 
-with open("dnsrecords.json", "r") as f: dns_cred = json.loads(f.read())
+with open("comprecords.json", "r") as f: COMP_DATA = json.load(f)
+with open("dnsrecords.json", "r") as f: DNS_CRED = json.load(f)
 
-flags = {}
+FLAGS = {}
+KILLPROG = False
+
+def steve(): # Jobs
+    last_save = time.time()
+    while True:
+        t = time.time()
+        for f in list(FLAGS):
+            if t - FLAGS[f]["time"] > 10:
+                FLAGS.pop(f)
+        if t - last_save > 30:
+            last_save = t
+            print(COMP_DATA)
+            with open("comprecords.json", "w+") as f: json.dump(COMP_DATA, f)
+        if KILLPROG:
+            break
 
 @app.route("/")
 def home():
@@ -25,18 +44,48 @@ def dns():
 def api_loghashrequest():
     name = request.form["name"]
     password = request.form["password"]
-    if name in dns_cred:
-        if bcrypt.checkpw(password.encode("UTF8"), dns_cred[name]["password"].encode("UTF8")):
+    if name in DNS_CRED:
+        if bcrypt.checkpw(password.encode("UTF8"), DNS_CRED[name]["password"].encode("UTF8")):
             num = random.randint(0, 2**64 - 1)
             response = {
                 "type": "gen_success",
                 "rand": num
             }
-            flags[num] = name
+            # FIX UP
+            h = sha512(name.encode("ASCII") + num.to_bytes(8, "little") + b"DUCKS").hexdigest()
+            FLAGS[h] = {
+                "name": name,
+                "rand": num,
+                "time": time.time()
+            }
             return jsonify(response)
         else:
             return jsonify({"type": "gen_badpassword"})
     else:
         return jsonify({"type": "gen_dnsnotregistered"})
 
+@app.route("/api/submithash", methods=["POST"])
+def api_submithash():
+    h = request.form["hash"]
+    name = request.form["name"]
+    password = request.form["password"]
+    if name in DNS_CRED:
+        if bcrypt.checkpw(password.encode("UTF8"), DNS_CRED[name]["password"].encode("UTF8")):
+            if h in FLAGS:
+                if FLAGS[h]["name"] != name:
+                    COMP_DATA[name]["points"] += 50
+                    return jsonify({"type": "points", "count": 50})
+                else:
+                    COMP_DATA[name]["points"] += 10
+                    return jsonify({"type": "points", "count": 10})
+            else:
+                print(FLAGS, h)
+                return jsonify({"type": "flag_invalid"})
+        else:
+            return jsonify({"type": "gen_badpassword"})
+    else:
+        return jsonify({"type": "gen_dnsnotregistered"})
+
+jobs = threading.Thread(target=steve, daemon=True)
+jobs.start()
 app.run("0.0.0.0", 8080)
